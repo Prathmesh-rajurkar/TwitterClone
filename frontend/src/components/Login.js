f93eac3e8a3c6795d9e80aa462a6562899123dfb
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2024-05-09 16:02:23 +0530

    frontend completed

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsTwitterX } from 'react-icons/bs'
+import { Link } from 'react-router-dom';
 
 function Login() {
+  const [isLogin, setIsLogin] = useState(false);
+  const loginSignupHandler = () => {
+    setIsLogin(!isLogin)
+  }
+
   return (
-    <div>Login</div>
+    <div className='w-screen h-screen flex items-center justify-center'>
+      <div className='flex items-center w-[80%] justify-evenly'>
+        <div className=''>
+          <BsTwitterX className='mx-4' size="300px" />
+        </div>
+        <div className='flex flex-col'>
+          <div className='font-bold text-5xl my-5'>
+          <h1>Happening now</h1>
+          </div>
+          <h1 className='mt-4  mb-2 text-2xl  font-bold'>{!isLogin ? "Register" : "Login"}</h1>
+          <form className='flex flex-col w-[70%]' action="">
+            {
+              !isLogin && (<>
+              <input type="text" className='outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-2 font-semibold' placeholder='Name' />
+              <input type="text" className='outline-blue-500  border border-gray-800 px-3 py-2 rounded-full my-2 font-semibold' placeholder='Username'/>  
+              </>)
+            }
+            <input type="text" className='outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-2 font-semibold'  placeholder='Email'/>
+            <input type="text" className='outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-2 font-semibold'  placeholder='Password'/>
+            <Link to="/" className='bg-[#1D9BF0] rounded-full p-2 my-4 text-lg text-white text-center '>{!isLogin ? "Register" : "Login"}</Link>
+            {
+
+            <h1>{isLogin ? "Do not have an account?":"Already have an account? "}<span className='cursor-pointer font-bold text-blue-500' onClick={loginSignupHandler}>{isLogin ? "Create Account" : "Login"}</span></h1>
+            }
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -9,7 +9,7 @@ function Profile() {
             <div className=''>
                 <div className=' px-2 py-2 flex items-center'>
                     <Link to="/" className='p-1 mx-1 rounded-full hover:bg-gray-200 cursor-pointer'>
-                        <IoArrowBackOutline size='20px'/>
+                        <IoArrowBackOutline size='20px' />
                     </Link>
                     <div>
                         <h1 className='font-bold text-lg'>Prathmesh Rajurkar</h1>
@@ -18,10 +18,17 @@ function Profile() {
                 </div>
                 <img src="https://images.unsplash.com/photo-1583520381351-5e28a12a1e69?1000x360" alt="banner" srcset="" />
                 <div className='border w-max border-white border-4 rounded-full absolute top-56 mx-3'>
-                 <Avatar githubHandle="sitebase" size={120} round={true} />
+                    <Avatar githubHandle="sitebase" size={120} round={true} />
                 </div>
-                <div>
+                <div className='text-right m-3'>
                     <button className='px-4 py-1 rounded-full text-right border border-gray-400'>Edit Profile</button>
+                </div>
+                <div className='m-4'>
+                    <h1 className='font-bold text-xl'>Prathmesh</h1>
+                    <p>@prathmsh1909</p>
+                </div>
+                <div className='m-4'>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque reprehenderit assumenda eos. Itaque numquam quia quod consequatur optio placeat animi, fuga mollitia repellendus perspiciatis maiores.</p>
                 </div>
             </div>
         </div>
